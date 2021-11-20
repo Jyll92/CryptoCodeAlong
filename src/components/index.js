3a355b129bd62f701a58586a30440a332301004c
Jyll92 <jyllanawright@gmail.com>
2021-11-20 17:11:46 -0500

    Initial Completion

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import { default as Navbar } from "./Navbar";
+import { default as Exchanges } from "./Exchanges";
+import { default as Homepage } from "./Homepage";
+import { default as Cryptocurrencies } from "./Cryptocurrencies";
+import { default as CryptoDetails } from "./CryptoDetails";
+import { default as News } from "./News";
+
+
+export { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News };
---
+++ src/services/cryptoNews.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '0ea6fb4ba5msh4a21c72af27e276p1d94b7jsnfbee2fff9402'
+  }
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers:cryptoNewsHeaders});
+
+export const cryptoNewsApi = createApi({
+    reducerPath: "crptoNewsApi",
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+});
+
+export const {
+    useGetCryptoNewsQuery
+} = cryptoNewsApi;
